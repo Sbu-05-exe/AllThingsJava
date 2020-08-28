2961e73f2615ae05018d8bd450b9d623b93f4916
Sibusiso <itsbudamain.05@gmail.com>
2020-08-28 23:03:20 +0200

    Done

--- SelectionSort.java
+++ SelectionSort.java
@@ -16,14 +16,40 @@ public class SelectionSort {
 
 	}
 
+	public static void selection (int[] list) {
+
+		int min_index, temp, i, j;
+
+		for (i = 0; i < list.length; i++) {
+
+			min_index = i;
+
+			for (j = i + 1; j < list.length; j++) {
+
+				if (list[j] < list[min_index]) {
+					
+					min_index = j;
+
+				}
+
+			}
+
+			temp = list[min_index];
+			list[min_index] = list[i];
+			list[i] = temp;
+
+		}
+	}
+
 	public static void main(String[] args) {
 
 		int[] data = mapToInt(args);
 		System.out.println(Arrays.toString(data));
+
+		selection(data);
+
 		System.out.println(Arrays.toString(data));
 
 	}
-
-
 
 }
